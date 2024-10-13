b52347cb6bb345d3ae765df36c2a5c70fe28d4c2
sand-script-s <sand.script.s@protonmail.com>
2024-10-13 17:32:22 +0200

    Add files via upload

--- scripts/script-backgroundchange.js
+++ scripts/script-backgroundchange.js
@@ -2,11 +2,10 @@ let backgrounddiv = document.getElementById('backgrounddiv');
 let backgroundArr = ['scripts/backgrounds/wall.jpg','scripts/backgrounds/bricks.jpg','scripts/backgrounds/stone.jpg','scripts/backgrounds/sand.jpg'];
 
 window.addEventListener('scroll', () => {
+    
     let scrollPosition = window.scrollY;
     let scrollHeightVar = document.body.scrollHeight;
-    
-    let tolerance = 50; /* Tolerance for dynamic viewvports */
-    let viewportHeight = window.innerHeight + tolerance;
+    let viewportHeight = window.innerHeight;
 
     let imageIndex = Math.floor((scrollPosition / (scrollHeightVar - viewportHeight)) * backgroundArr.length);
 
